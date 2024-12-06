d52766c461d056423fbfa608ba52d0c4a1213f87
NguyenVanKhai <nguyenkhai3571@gmail.com>
2024-12-07 00:21:37 +0700

    cap nhat news controller.php

--- Controller/NewsController.php
+++ Controller/NewsController.php
@@ -2,10 +2,13 @@
 require_once "models/News.php";
 
 class NewsController {
-    public function detail() {
-        $id = $_GET['id'];
-        $news = News::getById($id);
+    public function index() {
+        $news = News::getAll();
+        include "views/home/index.php";
+    }
 
+    public function detail($id) {
+        $news = News::getById($id);
         include "views/news/detail.php";
     }
 }
